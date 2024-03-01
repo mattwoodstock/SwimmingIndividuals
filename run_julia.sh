743764576f68bb171e00508aadac71391a2657c4
Matt Woodstock <58792057+fishesofthedeep@users.noreply.github.com>
2024-03-01 09:48:51 -0500

    Update run_julia.sh

--- run_julia.sh
+++ run_julia.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=matthew.woodstock@whoi.edu  # Where to send mail
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=8	# Nubmer of CPU cores per task
-#SBATCH --mem=5gb                   # Job memory request
+#SBATCH --mem=1gb                   # Job memory request
 #SBATCH --time=24:00:00             # Time limit hrs:min:sec
 #SBATCH --output=serial_job_%j.err  # Standard error
 #SBATCH --output=serial_job_%j.out  # Standard output
@@ -16,6 +16,6 @@ module load julia                  # Load the julia module
  
 echo "Running julia script for SwimmingIndividuals.jl"
  
-julia /vortexfs1/scratch/username/SwimmingIndividuals/model.jl
+julia /vortexfs1/scratch/matthew.woodstock/SwimmingIndividuals/model.jl
  
 date
