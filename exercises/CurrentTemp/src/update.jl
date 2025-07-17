9520b96270d1dfcd46806e7bdf15e7bca36b24f2
Matt Woodstock <58792057+mattwoodstock@users.noreply.github.com>
2025-07-17 09:59:40 -0400

    Delete exercises directory

--- exercises/CurrentTemp/src/update.jl
+++
@@ -1,6 +0,0 @@
-function update!(sim::MarineSimulation; time_offset = (vels = false, PARF = false, temp = false))
-    @info "Starting model..."
-    for i in 1:sim.iterations
-        TimeStep!(sim)
-    end
-end
